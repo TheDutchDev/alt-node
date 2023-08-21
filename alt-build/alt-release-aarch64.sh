f18711a225a5d1bb94f20ba41239548fa3b2cda8
Niels Visbeek <nielsvisbeek@outlook.com>
2023-08-21 20:05:10 +0200

    add sudo for workflow. adjust workflow for tests.

--- alt-build/alt-release-aarch64.sh
+++ alt-build/alt-release-aarch64.sh
@@ -1,5 +1,5 @@
 cd ..
-make clean
+sudo make clean
 touch ./deps/base64/base64/lib/config.h
 
 # Not all of these may be required, but I suppose it can't hurt to have them...
@@ -13,5 +13,5 @@ export CC_host="aarch64-linux-gnu-gcc"
 export CXX_host="aarch64-linux-gnu-g++"
 
 ./configure --shared --dest-cpu=arm64 --cross-compiling --dest-os=linux --with-arm-float-abi=hard --with-arm-fpu=neon
-make -j$(nproc)
+sudo make -j$(nproc)
 cd alt-build
